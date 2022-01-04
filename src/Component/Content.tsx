984429b41aca26ff3821bb1450d7f544d642f110
gca98 <giovannichristian98@gmail.com>
2022-01-04 16:27:54 +0700

    sesi23

---
+++ src/Component/Content.tsx
@@ -0,0 +1,14 @@
+
+
+function Content(){
+    return (
+    <div className="p-5 mb-4 bg-light rounded-3 border">
+        <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold">First Paragraoh in Jumbotron</h1>
+            <p className="col-md-8 fs-4">using a ...</p>
+            <button className="btn btn-primary btn-lg">Example button</button>
+        </div>
+    </div>)
+}
+
+export default Content
---
+++ src/Component/Footer.tsx
@@ -0,0 +1,13 @@
+
+
+function Footer(){
+    return(
+        <footer className="text-muted py-5 border-top">
+            <div className="container">
+                <p className="mb-1"> This is My First React App in Bootstrap</p>
+                <p className="mb-0"> new to react? <a href="https://reactjs.org/">Visit the HomePage</a></p>
+            </div>
+        </footer>
+    )
+}
+export default Footer
